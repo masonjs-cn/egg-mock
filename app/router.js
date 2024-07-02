7a1165f0c525166f638e0a43ca620fc2e23902b3
Mason <954663633@qq.com>
2024-07-02 13:12:44 +0800

    fix eslint

--- app/router.js
+++ app/router.js
@@ -8,19 +8,19 @@ module.exports = (app) => {
   router.get('/', controller.home.index)
 
   // R 读取（Read）
-  router.get("/user", controller.user.read);
+  router.get('/user', controller.user.read)
 
   // R 读取（Read）
-  router.get("/user/:id", controller.user.readId);
+  router.get('/user/:id', controller.user.readId)
 
   // C 创建（Create）
-  router.post("/user", controller.user.add);
+  router.post('/user', controller.user.add)
 
   // D 删除（Delete）
-  router.delete("/user/:id", controller.user.delete);
+  router.delete('/user/:id', controller.user.delete)
 
   // U 更新（Update）
-  router.put("/user", controller.user.update);
+  router.put('/user', controller.user.update)
 
   // 下载
   router.get('/downloadTemp', controller.user.downloadTemp)
--- app/service/user.js
+++ app/service/user.js
@@ -1,48 +1,44 @@
 const Service = require('egg').Service
-const dayjs = require('dayjs')
-const randomBoolean = require('../utils/random')
-const { readFile } = require('fs')
-const { join } = require('path')
 
 class UserService extends Service {
   // 读取所有的数据
   async read() {
     try {
-      return await this.ctx.helper.query("user");
+      return await this.ctx.helper.query('user')
     } catch (error) {
-      return error;
+      return error
     }
   }
 
   async readId(id) {
-      try {
-        return await this.ctx.helper.queryId("user", id);
-      } catch (error) {
-        return error;
-      }
+    try {
+      return await this.ctx.helper.queryId('user', id)
+    } catch (error) {
+      return error
+    }
   }
 
   async add(row) {
     try {
-      return await this.ctx.helper.add("user", row);
+      return await this.ctx.helper.add('user', row)
     } catch (error) {
-      return error;
+      return error
     }
   }
 
   async del(id) {
     try {
-      return await this.ctx.helper.queryId("user", id);
+      return await this.ctx.helper.queryId('user', id)
     } catch (error) {
-      return error;
+      return error
     }
   }
 
   async update(row) {
     try {
-      return await this.ctx.helper.update("user", row);
+      return await this.ctx.helper.update('user', row)
     } catch (error) {
-      return error;
+      return error
     }
   }
 }
